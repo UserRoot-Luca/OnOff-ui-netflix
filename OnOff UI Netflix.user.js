9bb1c7715ddd96352cc2b37ed916f281522203fe
UserRoot-Luca <55756898+UserRoot-Luca@users.noreply.github.com>
2022-11-19 16:03:33 +0100

    V 1.2

--- OnOff UI Netflix.user.js
+++ OnOff UI Netflix.user.js
@@ -2,7 +2,7 @@
 // ==UserScript==
 // @name         On Off UI Netflix
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.2
 // @description  ###
 // @author       UserRoot-Luca
 // @match        https://www.netflix.com/*
@@ -11,29 +11,40 @@
 // @run-at       document-end
 // ==/UserScript==
 (function () {
-    document.onload = () => {
+    window.onload = () => {
         const KEY = "u";
         let switchUI = true;
         window.addEventListener('keydown', (e) => {
             if (e.key == KEY) {
                 if (switchUI) {
+                    console.log("UI OFF");
                     switchUI = false;
                 }
                 else {
+                    console.log("UI On");
                     switchUI = true;
                 }
             }
         }, false);
-        document.querySelector("[data-uia=\"player\"]").addEventListener("DOMSubtreeModified", () => {
-            const MyElement = document.evaluate("//*[@id=\"appMountPoint\"]/div/div/div[1]/div/div[2]/div[2]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-            if (MyElement != null) {
-                if (!switchUI) {
-                    MyElement.style.display = "none";
-                }
-                else {
-                    MyElement.style.display = "";
-                }
+        let E_player = document.querySelector("[data-uia=\"player\"]");
+        let GetPlayerInterval = setInterval(() => {
+            if (E_player == null) {
+                E_player = document.querySelector("[data-uia=\"player\"]");
             }
-        });
+            else {
+                E_player.addEventListener("DOMSubtreeModified", () => {
+                    const MyElement = document.evaluate("//*[@id=\"appMountPoint\"]/div/div/div[1]/div/div[2]/div[2]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+                    if (MyElement != null) {
+                        if (!switchUI) {
+                            MyElement.style.display = "none";
+                        }
+                        else {
+                            MyElement.style.display = "";
+                        }
+                    }
+                });
+                clearInterval(GetPlayerInterval);
+            }
+        }, 300);
     };
 })();
--- OnOff UI Netflix.user.ts
+++ OnOff UI Netflix.user.ts
@@ -1,8 +1,7 @@
-
 // ==UserScript==
 // @name         On Off UI Netflix
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.2
 // @description  ###
 // @author       UserRoot-Luca
 // @match        https://www.netflix.com/*
@@ -12,33 +11,43 @@
 // ==/UserScript==
 
 (function () {
-    document.onload = () => {
+    window.onload = () => {
         const KEY = "u";
         let switchUI = true;
         window.addEventListener('keydown', (e) => {
             if (e.key == KEY) {
                 if (switchUI) {
+                    console.log("UI OFF");
                     switchUI = false;
                 } else {
+                    console.log("UI On");
                     switchUI = true;
                 }
             }
         }, false);
-        document.querySelector<HTMLDivElement>("[data-uia=\"player\"]")!.addEventListener("DOMSubtreeModified", () => {
-            const MyElement:any = document.evaluate(
-                "//*[@id=\"appMountPoint\"]/div/div/div[1]/div/div[2]/div[2]",
-                document,
-                null,
-                XPathResult.FIRST_ORDERED_NODE_TYPE,
-                null,
-            ).singleNodeValue;
-            if (MyElement != null) {
-                if (!switchUI) {
-                    MyElement.style.display = "none";
-                } else {
-                    MyElement.style.display = "";
-                }
+        let E_player = document.querySelector<HTMLDivElement>("[data-uia=\"player\"]");
+        let GetPlayerInterval = setInterval(() => {
+            if (E_player == null) {
+                E_player = document.querySelector<HTMLDivElement>("[data-uia=\"player\"]");
+            } else {
+                E_player.addEventListener("DOMSubtreeModified", () => {
+                    const MyElement:any = document.evaluate(
+                        "//*[@id=\"appMountPoint\"]/div/div/div[1]/div/div[2]/div[2]",
+                        document,
+                        null,
+                        XPathResult.FIRST_ORDERED_NODE_TYPE,
+                        null,
+                    ).singleNodeValue;
+                    if (MyElement != null) {
+                        if (!switchUI) {
+                            MyElement.style.display = "none";
+                        } else {
+                            MyElement.style.display = "";
+                        }
+                    }
+                })
+                clearInterval(GetPlayerInterval);
             }
-        })
+        }, 300)
     }
 })();
