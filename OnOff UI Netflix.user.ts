ce04a30a879dd9d648b159daa11ee2af841d7e48
UserRoot-Luca <desiro.luca@gmail.com>
2024-04-22 15:12:47 +0200

    V 2.3

--- OnOff UI Netflix.user.ts
+++ OnOff UI Netflix.user.ts
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         On Off UI Netflix
 // @namespace    http://tampermonkey.net/
-// @version      2.2
+// @version      2.3
 // @description  ###
 // @author       UserRoot-Luca
 // @match        https://www.netflix.com/*
@@ -57,7 +57,7 @@
             } else {
                 E_player.addEventListener("DOMSubtreeModified", () => {
                     const MyElement:any = document.evaluate(
-                        "//*[@id=\"appMountPoint\"]/div/div/div[1]/div/div[2]/div[2]",
+                        "//*[@id=\"appMountPoint\"]/div/div/div[1]/div/div/div[2]/div[2]",
                         document,
                         null,
                         XPathResult.FIRST_ORDERED_NODE_TYPE,
