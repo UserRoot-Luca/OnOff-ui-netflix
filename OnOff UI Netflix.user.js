99d2edfe1b2e94a1a10fe3288f9df3fabba19354
UserRoot-Luca <desiro.luca@gmail.com>
2024-10-25 14:19:52 +0200

    V 3.0

--- OnOff UI Netflix.user.js
+++ OnOff UI Netflix.user.js
@@ -2,7 +2,7 @@
 // ==UserScript==
 // @name         On Off UI Netflix
 // @namespace    http://tampermonkey.net/
-// @version      2.4
+// @version      3.0
 // @description  ###
 // @author       UserRoot-Luca
 // @match        https://www.netflix.com/*
@@ -40,10 +40,7 @@
             }
         }, false);
         const TimeMultiplier = (seconds, speed) => {
-            if (speed >= 1) {
-                return seconds / speed;
-            }
-            return seconds;
+            return seconds / speed;
         };
         const TimeFormats = (seconds, speed) => {
             let s = TimeMultiplier(seconds, speed);
--- OnOff UI Netflix.user.ts
+++ OnOff UI Netflix.user.ts
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         On Off UI Netflix
 // @namespace    http://tampermonkey.net/
-// @version      2.4
+// @version      3.0
 // @description  ###
 // @author       UserRoot-Luca
 // @match        https://www.netflix.com/*
@@ -38,10 +38,7 @@
             }
         }, false);
         const TimeMultiplier = (seconds:number, speed:number):number => {
-            if(speed >= 1) {
-                return seconds / speed;
-            }
-            return seconds
+            return seconds / speed;
         }
         const TimeFormats = (seconds:number, speed:number):string => {
             let s:number = TimeMultiplier(seconds, speed);
